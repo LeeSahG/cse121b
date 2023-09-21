1bf41d34bfbf5017fa8fa671472761ff3a26a2fb
LeeSahG <ilangagasa@gmail.com>
2023-09-21 15:34:18 +0200

    updates for resubmission

---
+++ scripts/w02-task.js
@@ -0,0 +1,31 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Lisakhanya Gasa";
+let currentYear = 2023;
+let profilePicture = "images/profile.jpg";
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+
+
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
