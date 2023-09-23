d14c7150f78eb4a22756c8199816a1c8362c7f03
LeeSahG <ilangagasa@gmail.com>
2023-09-23 21:02:41 +0200

    getElementById updates

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -11,21 +11,18 @@ let profilePicture = "images/profile.jpg";
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
-
-
+const imageElement = document.getElementById("image");
 
 
 /* Step 4 - Adding Content */
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = fullName;
+imageElement.setAttribute("src", profilePicture);
 
 
 
 /* Step 5 - Array */
-
-
-
+let favoriteFoods = ["pizza", "ribs", "fried chips"];
 
 
 
